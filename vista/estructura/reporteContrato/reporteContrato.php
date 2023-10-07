7227e696539e9d16680e35b4f624ac48e2227edb
SC131 <amos27109@gmail.com>
2023-10-07 12:41:16 -0500

    Reporte Ordenes

---
+++ vista/estructura/reporteContrato/reporteContrato.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporte de Contrato</title>
+    <link rel="stylesheet" href="../../estilos/reporteContratoStyle/reporteContrato.css">
+</head>
+
+<body>
+    <div class="report-c">
+        <header class="app-bar">
+            <div class="toolbar-title">
+                <img src="../../../Img/Kallpa.png" alt="Kallpa" class="kallpa-image">
+            </div>
+            <div class="spacer"></div>
+            <img src="../../../Img/usuario (3).png" alt="Usuario" class="user-image">
+        </header>
+
+        <footer class="footer">
+            <div class="text-footer">
+                &copy; 2023 KALLPA. Todos los derechos reservados.
+            </div>
+        </footer>
+
+        <div style="margin-top: 28px">
+            <h1 class="text-center titulo">
+                <span style="color: #32cc32">REPORTE</span>
+                <span style="color: #203864"> - CONTRATO</span>
+            </h1>
+        </div>
+
+        <div class="floating-rectangle">
+            <h2 class="texto-area">
+                <span class="texto-area1">Reporte de</span>
+                <span class="texto-area2">Contrato</span>
+            </h2>
+
+            <textarea id="reporteContratos" class="txtarea" disabled></textarea>
+        </div>
+        <div class="contenedor-cajas-F ">
+            <h2 class="texto-center">Fecha</h2>
+            <div class="date-picker">
+                <!-- Inserta aquí tus date pickers -->
+            </div>
+        </div>
+        <div class="filtros">
+            <h2>Filtros</h2>
+
+            <div>
+                <p>Estado Contrato</p>
+                <div>
+                    <!-- Inserta aquí tus opciones de radio buttons -->
+                </div>
+            </div>
+
+            <div>
+                <p>Asesor</p>
+                <div>
+                    <!-- Inserta aquí tu combobox -->
+                </div>
+            </div>
+
+            <div>
+                <button onclick="generarReporte()" class="btn-container btn-generar">Generar Reporte</button>
+                <button onclick="atras()" class="btn-container btn-atras">Atras</button>
+            </div>
+        </div>
+
+        <div class="tabla fill-height ">
+            <table class="custom-table">
+                <thead>
+                    <tr>
+                        <!-- Inserta aquí tus encabezados de tabla -->
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Inserta aquí tus filas de tabla -->
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</body>
+
+</html>
