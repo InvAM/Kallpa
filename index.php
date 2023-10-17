db7431339b4afa1b2f22173f69571c047c39113c
SC131 <amos27109@gmail.com>
2023-10-16 22:45:29 -0500

    Creacion de la configuracion de index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errores', true);
+ini_set('display_errors', FALSE);
+ini_set('log_erroes', TRUE);
+ini_set("error_log", "php-error.log");
+error_log("Inicio de aplicacion web");
+
+require_once "libs/app.php";
