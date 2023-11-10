f0ac61fbbe304b8c9984028fb8bdee5677ddc019
SC131 <amos27109@gmail.com>
2023-11-10 16:49:54 -0500

    Funciona antes de bajar cambios de carlos 10/11/2023

--- views/menu/menu.php
+++ views/menu/menu.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="public/Img/KallpaC.png" type="image/x-icon">
-    <link rel="stylesheet" href="public/css/menu.css">
+    <link rel="stylesheet" href="<?php echo constant('URL') ?>public/css/menu.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css">
     <title>¡Bienvenido!</title>
 </head>
