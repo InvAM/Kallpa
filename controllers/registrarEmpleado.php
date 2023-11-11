4f00bf751376d4d0c682230c2dc4a45dfc1e6ab5
SC131 <amos27109@gmail.com>
2023-11-11 11:11:30 -0500

    Ajax para actualizar empleado en la misma pagina

--- controllers/registrarEmpleado.php
+++ controllers/registrarEmpleado.php
@@ -55,31 +55,36 @@ class RegistrarEmpleado extends Controller
     function actualizarEmpleado()
     {
         session_start();
-        $dni = $_SESSION['dni_verEmpleado'];
+        // Obtén los datos del formulario
+        $dni = $_POST['DNI_Em_reg'];
         $nom = $_POST['Nombre_Em_reg'];
         $ape = $_POST['Apellido_Em_reg'];
         $cel = $_POST['Celular_Em_reg'];
         $categoria = $_POST['IDCategoria_reg'];
 
-        unset($_SESSION['dni_verEmpleado']);
-        if ($this->model->update(['DNI_Em_reg' => $dni, 'Nombre_Em_reg' => $nom, 'Apellido_Em_reg' => $ape, 'Celular_Em_reg' => $cel, 'IDCategoria_reg' => $categoria])) {
-            //actualizar empleado
-            $empleado = new Empleado();
-            $empleado->DNI_Em = $dni;
-            $empleado->Nombre_Em = $nom;
-            $empleado->Apellido_Em = $ape;
-            $empleado->Celular_Em = $cel;
-            $empleado->IDCategoria = $categoria;
+        // Asegúrate de que los datos estén presentes y válidos
+        if (!empty($dni) && !empty($nom) && !empty($ape) && !empty($cel) && !empty($categoria)) {
+            // Actualiza el empleado en el modelo
+            if ($this->model->update(['DNI_Em_reg' => $dni, 'Nombre_Em_reg' => $nom, 'Apellido_Em_reg' => $ape, 'Celular_Em_reg' => $cel, 'IDCategoria_reg' => $categoria])) {
+                // Crea un objeto Empleado con los datos actualizados
+                $empleado = new Empleado();
+                $empleado->DNI_Em = $dni;
+                $empleado->Nombre_Em = $nom;
+                $empleado->Apellido_Em = $ape;
+                $empleado->Celular_Em = $cel;
+                $empleado->IDCategoria = $categoria;
 
-            $this->view->empleado = $empleado;
-            $this->view->mensaje = "Empleado Actualizado Correctamente";
+                // Define la URL de redirección
+                $redirectUrl = constant('URL') . 'registrarEmpleado'; // Cambia 'tu_pagina_actual' por la URL real
 
-        } else {
-            //msg de error
-            $this->view->mensaje = "No se puedo actualizar em empleado";
+                // Devuelve una respuesta JSON con éxito, la URL de redirección y el objeto Empleado
+                echo json_encode(['success' => true, 'redirect' => $redirectUrl, 'empleado' => $empleado, 'mensaje' => 'Empleado Actualizado Correctamente']);
+                return;
+            }
         }
-        $this->view->render('registrarEmpleado/formDetalleEmpleado');
+        echo json_encode(['success' => false, 'mensaje' => 'No se pudo actualizar el empleado']);
     }
+
 
     function eliminarEmpleado($param = null)
     {
