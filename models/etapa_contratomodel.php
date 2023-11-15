18febfb4a2a5931fa74c626cdf9bf04fe3dc7f4b
SC131 <amos27109@gmail.com>
2023-11-14 21:29:25 -0500

    Cambios idk

--- models/etapa_contratomodel.php
+++ models/etapa_contratomodel.php
@@ -1,81 +1,87 @@
 <?php
 include_once "models/etapa_contrato.php";
 
-class Etapa_ContratoModel extends Model {
+class Etapa_ContratoModel extends Model
+{
 
-    public function __construct(){
-        parent:: __construct();
+    public function __construct()
+    {
+        parent::__construct();
     }
 
-    public function insert($datos){
-        try{
+    public function insert($datos)
+    {
+        try {
             $query = $this->db->connect()->prepare('INSERT INTO etapa_contrato (IDContrato, IDEtapa,DNI_Em,Fecha_Et)
             VALUES (:idcontrato,:idetapa,:dni_em,:fecha)');
             $query->execute([
-                 'idcontrato'=> $datos['IDContrato'],
-                 'idetapa '=> $datos['IDEtapa'],
-                 'dni_em' => $datos['DNI_Em'],
-                 'fecha' => $datos['Fecha_Et']
+                'idcontrato' => $datos['IDContrato'],
+                'idetapa ' => $datos['IDEtapa'],
+                'dni_em' => $datos['DNI_Em'],
+                'fecha' => $datos['Fecha_Et']
             ]);
             return true;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             return false;
         }
     }
 
-    public function get(){
+    public function get()
+    {
         $etapascontratos = [];
-        try{
-            $query = $this-> db ->connect() ->prepare ('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato');
-            $query ->execute();
-            while($row = $query ->fetch()){
+        try {
+            $query = $this->db->connect()->prepare('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato');
+            $query->execute();
+            while ($row = $query->fetch()) {
                 $etapacontrato = new Etapa_Contrato();
-                $etapacontrato->IDContrato=$row['IDContrato'];
-                $etapacontrato->IDEtapa=$row['IDEtapa'];
+                $etapacontrato->IDContrato = $row['IDContrato'];
+                $etapacontrato->IDEtapa = $row['IDEtapa'];
                 $etapacontrato->DNI_Em = $row['DNI_Em'];
-                $etapacontrato->Fecha_Et=$row['Fecha_Et'];
+                $etapacontrato->Fecha_Et = $row['Fecha_Et'];
                 array_push($etapascontratos, $etapacontrato);
             }
             return $etapascontratos;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             return [];
         }
     }
 
-    public function getInstalacion(){
+    public function getInstalacion()
+    {
         $etapascontratos = [];
-        try{
-            $query = $this-> db ->connect() ->prepare ('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato WHERE IDEtapa=1');
-            $query ->execute();
-            while($row = $query ->fetch()){
+        try {
+            $query = $this->db->connect()->prepare('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato WHERE IDEtapa=1');
+            $query->execute();
+            while ($row = $query->fetch()) {
                 $etapacontrato = new Etapa_Contrato();
-                $etapacontrato->IDContrato=$row['IDContrato'];
-                $etapacontrato->IDEtapa=$row['IDEtapa'];
+                $etapacontrato->IDContrato = $row['IDContrato'];
+                $etapacontrato->IDEtapa = $row['IDEtapa'];
                 $etapacontrato->DNI_Em = $row['DNI_Em'];
-                $etapacontrato->Fecha_Et=$row['Fecha_Et'];
+                $etapacontrato->Fecha_Et = $row['Fecha_Et'];
                 array_push($etapascontratos, $etapacontrato);
             }
             return $etapascontratos;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             return [];
         }
     }
 
-    public function getHabilitacion(){
+    public function getHabilitacion()
+    {
         $etapascontratos = [];
-        try{
-            $query = $this-> db ->connect() ->prepare ('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato WHERE IDEtapa=2');
-            $query ->execute();
-            while($row = $query ->fetch()){
+        try {
+            $query = $this->db->connect()->prepare('SELECT IDContrato,IDEtapa,DNI_Em,Fecha_Et FROM etapa_contrato WHERE IDEtapa=2');
+            $query->execute();
+            while ($row = $query->fetch()) {
                 $etapacontrato = new Etapa_Contrato();
-                $etapacontrato->IDContrato=$row['IDContrato'];
-                $etapacontrato->IDEtapa=$row['IDEtapa'];
+                $etapacontrato->IDContrato = $row['IDContrato'];
+                $etapacontrato->IDEtapa = $row['IDEtapa'];
                 $etapacontrato->DNI_Em = $row['DNI_Em'];
-                $etapacontrato->Fecha_Et=$row['Fecha_Et'];
+                $etapacontrato->Fecha_Et = $row['Fecha_Et'];
                 array_push($etapascontratos, $etapacontrato);
             }
             return $etapascontratos;
-        }catch (PDOException $e){
+        } catch (PDOException $e) {
             return [];
         }
     }
--- public/js/generarOrdenI.js
+++ public/js/generarOrdenI.js
@@ -1,101 +1,60 @@
-
 function iniciar() {
-    if (localStorage.getItem('DNI_Em_T')||localStorage.getItem('numSum')) {
-        // Obtener los datos del localStorage de TECNICO
-        var dni = localStorage.getItem('DNI_Em_T');
-        var nombre = localStorage.getItem('Nombre_Em_T');
-        var apellido = localStorage.getItem('Apellido_Em_T');
-        if(localStorage.getItem('Nombre_Em_T')){
-        var nombreCompleto = nombre + " " + apellido;
-        }else{
-            var nombreCompleto = "";
-        }
-        
-        //Obtener los datos del localStorage de CONTRATO
-        var numS = localStorage.getItem('numSum');
-        var idContrato= localStorage.getItem('IDContrato');
+	if (localStorage.getItem("DNI_Em_T") || localStorage.getItem("numSum")) {
+		// Obtener los datos del localStorage de TECNICO
+		var dni = localStorage.getItem("DNI_Em_T");
+		var nombre = localStorage.getItem("Nombre_Em_T");
+		var apellido = localStorage.getItem("Apellido_Em_T");
+		if (localStorage.getItem("Nombre_Em_T")) {
+			var nombreCompleto = nombre + " " + apellido;
+		} else {
+			var nombreCompleto = "";
+		}
 
-        // Asignar los valores a los campos del formulario
-        var numO= "OI-"+idContrato;
-        
-        console.log(numO);
-        $("input[name='numOrden_G']").val(numO);
-        $("input[name='IDContrato_G']").val(idContrato);
-        $("input[name='NumS_G']").val(numS);
-        $("input[name='DNI_Em_T']").val(dni);
-        $("input[name='NombreCompleto_Em']").val(nombreCompleto);
-    }
+		//Obtener los datos del localStorage de CONTRATO
+		var numS = localStorage.getItem("numSum");
+		var idContrato = localStorage.getItem("IDContrato");
+
+		// Asignar los valores a los campos del formulario
+		var numO = "OI-" + idContrato;
+
+		console.log(numO);
+		$("input[name='numOrden_G']").val(numO);
+		$("input[name='IDContrato_G']").val(idContrato);
+		$("input[name='NumS_G']").val(numS);
+		$("input[name='DNI_Em_T']").val(dni);
+		$("input[name='NombreCompleto_Em']").val(nombreCompleto);
+	}
 }
 
 function handleDateSelection(event) {
-    // Obtén el valor seleccionado
-    var selectedDate = event.target.value;
+	// Obtén el valor seleccionado
+	var selectedDate = event.target.value;
 
-    console.log("Fecha seleccionada:", selectedDate);
-
+	console.log("Fecha seleccionada:", selectedDate);
 }
 
-
-document.addEventListener('DOMContentLoaded', iniciar);
+document.addEventListener("DOMContentLoaded", iniciar);
 
 $(document).ready(function () {
-    $('#formularioGOI').submit(function (event) {
-        event.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
-
-        // Recopila los valores de los campos utilizando jQuery
-        var IDEtapa_G = $('#IDEtapa_G').val();
-        var IDContrato_G = $('#IDContrato_G').val();
-        var Fecha = $('#selectedDate').val();
-        var DNI_Em_T = $('#DNI_Em_T').val();
-
-        ordenActual=[];
-        //Llenando arreglo
-        var ordenActual = {
-            IDEtapa_G: IDEtapa_G,
-            IDContrato_G: IDContrato_G,
-            Fecha: Fecha,
-            DNI_Em_T: DNI_Em_T
-        };
-        //Comprobando 
-        console.log(ordenActual)
-        
-        // Envía el formulario
-        $.ajax({
-            url: 'generarOrdenI/registrarOrden',  // Reemplaza 'tu_controlador_url' con la URL de tu controlador
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(ordenActual),
-            success: function(response) {
-                // Manejar la respuesta del servidor (si es necesario)
-                console.log(response);
-            },
-            error: function(error) {
-                // Manejar errores
-                console.error(error);
-            }
-        })
-        $(this).off('submit');
-        $(this).submit();
-    });
+	$("#formularioGOI").submit(function (event) {});
 });
 
-
-$(document).ready(function(){
-   //AgregarTécnico
-   $('#btnAgregarTecnico').on('click', function () {
-    //Direccionando a otra página
-    window.location.href = 'tecnico';
-    exit();
-   });
-   //Volver
-   $('#btnVolver').on('click', function () {
-     //Direccionando a otra página
-     window.location.href = 'consultarContrato';
-     exit();
-     });
-   //limpiar
-   $('#btnLimpiar').on('click',function(){
-    $("input[name='DNI_Em_T']").val("");
-    $("input[name='NombreCompleto_Em']").val("");
-   });
+$(document).ready(function () {
+	//AgregarTécnico
+	$("#btnAgregarTecnico").on("click", function () {
+		//Direccionando a otra página
+		window.location.href = "tecnico";
+		exit();
+	});
+	//Volver
+	$("#btnVolver").on("click", function () {
+		//Direccionando a otra página
+		window.location.href = "consultarContrato";
+		exit();
+	});
+	//limpiar
+	$("#btnLimpiar").on("click", function () {
+		$("input[name='DNI_Em_T']").val("");
+		$("input[name='NombreCompleto_Em']").val("");
+	});
 });
