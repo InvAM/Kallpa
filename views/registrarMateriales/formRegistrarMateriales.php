55d6bd4eed37c537f8c7811c7bd72d261ff5c518
SC131 <amos27109@gmail.com>
2023-11-16 19:43:48 -0500

    Modulo de Registrar Material

--- views/registrarMateriales/formRegistrarMateriales.php
+++ views/registrarMateriales/formRegistrarMateriales.php
@@ -24,18 +24,21 @@
                     <p class="tituloS2">Especificaciones de contrato</p>
                     <div class="cajaContratoP">
                         <div class="subcajita">
-                        <label>ID Contrato<label><br>
-                        <input type="text" label="idContrato" id="IDContrato_M" name="IDContrato_M" placeholder="ID Contrato">
+                            <label>ID Contrato<label><br>
+                                    <input type="text" label="idContrato" id="IDContrato_M" name="IDContrato_M"
+                                        placeholder="ID Contrato">
                         </div>
                         <div class="subcajita">
-                        <label>Fecha Orden<label><br>
-                        <input type="text" label="fecha" id="Fecha_O" name="Fecha_O" placeholder="Fecha de Orden">
+                            <label>Fecha Orden<label><br>
+                                    <input type="text" label="fecha" id="Fecha_O" name="Fecha_O"
+                                        placeholder="Fecha de Orden">
                         </div>
                         <div class="subcajita">
-                        <label>ID Etapa<label><br>
-                        <input type="text" label="idEtapa" id="IDEtapa_M" name="IDEtapa_M"placeholder="ID Etapa">
+                            <label>ID Etapa<label><br>
+                                    <input type="text" label="idEtapa" id="IDEtapa_M" name="IDEtapa_M"
+                                        placeholder="ID Etapa">
                         </div>
-                    </div> 
+                    </div>
                 </div>
             </div>
         </div>
@@ -45,48 +48,52 @@
             <div class="CajaMaterial">
                 <p><Strong>Registrar Materiales</Strong></p>
                 <label>Material<label>
-                <select  id="materialSelect">
-                <?php 
-                 include_once 'models/material.php';
-                foreach ($this->material as $opcion) { 
-                    $material = new Material();
-                    $material =$opcion;  ?>
-                      <option value="<?php echo $opcion->Nombre_Ma; ?>"><?php echo $opcion->Nombre_Ma; ?></option>
-                <?php } ?>
-                </select>
-                <label>Cantidad<label>
-                <input class="n1" type="number" name="Cantidad_Ma" id="Cantidad_Ma" label="cantidad" placeholder="Cantidad">
-                <button class="boton" id="btnAgregar">
-                    Añadir
-                    <i class="mdi mdi-plus"></i>
-                </button>
-                <button class="boton" id="btnActualizar">
-                    Actualizar
-                    <i class="mdi mdi-pencil"></i>
-                </button>
+                        <select id="materialSelect">
+                            <?php
+                            include_once 'models/material.php';
+                            foreach ($this->material as $opcion) {
+                                $material = new Material();
+                                $material = $opcion; ?>
+                                <option value="<?php echo $opcion->Nombre_Ma; ?>">
+                                    <?php echo $opcion->Nombre_Ma; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                        <label>Cantidad<label>
+                                <input class="n1" type="number" name="Cantidad_Ma" id="Cantidad_Ma" label="cantidad"
+                                    placeholder="Cantidad">
+                                <button class="boton" id="btnAgregar">
+                                    Añadir
+                                    <i class="mdi mdi-plus"></i>
+                                </button>
+                                <button class="boton" id="btnActualizar">
+                                    Actualizar
+                                    <i class="mdi mdi-pencil"></i>
+                                </button>
             </div>
             <div class="CajaTabla">
                 <p><strong>Detalle Materiales</strong></p>
-                <form action="<?php echo constant('URL'); ?>registrarMateriales/registrarMateriales" class="formularioRM"name="formularioRM" id="formularioRM" method="POST">
-                <div class="tabla">
-                    <table id="tablaMateriales" class="custom-table">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Material</th>
-                                <th>Cantidad</th>
-                                <th>Seleccionar</th>
-                                <th>Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <button class="boton-opciones" id="btnAsignarMaterialesBD"> Asignar Materiales
-                    <i class="mdi mdi-book-plus"></i></button>
+                <form action="<?php echo constant('URL'); ?>registrarMateriales/registrarMateriales"
+                    class="formularioRM" name="formularioRM" id="formularioRM" method="POST">
+                    <div class="tabla">
+                        <table id="tablaMateriales" class="custom-table">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Material</th>
+                                    <th>Cantidad</th>
+                                    <th>Seleccionar</th>
+                                    <th>Eliminar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <button class="boton-opciones" id="btnAsignarMaterialesBD"> Asignar Materiales
+                        <i class="mdi mdi-book-plus"></i></button>
                 </form>
                 <button class="boton-opciones" id="btnLimpiar"> Limpiar Lista
                     <i class="mdi mdi-restore"></i></button>
