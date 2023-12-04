0339b7567935d91246dc185e160a84f8ea96aa3a
SC131 <amos27109@gmail.com>
2023-12-03 22:14:59 -0500

    Buscar cliente setear con ajax pos

--- public/js/registrarContrato.js
+++ public/js/registrarContrato.js
@@ -1,160 +1,165 @@
 function handleDateSelection(event) {
-  // Obtén el valor seleccionado
-  var selectedDate = event.target.value;
+	// Obtén el valor seleccionado
+	var selectedDate = event.target.value;
 
-  console.log("Fecha seleccionada:", selectedDate);
+	console.log("Fecha seleccionada:", selectedDate);
 }
 
 $(document).ready(function () {
-  //VOLVER
-  $("#btnAtras").on("click", function () {
-    window.location.href = "menu";
-  });
+	//VOLVER
+	$("#btnAtras").on("click", function () {
+		window.location.href = "menu";
+	});
 
-  //
-  $("#imprimir").on("click", function () {
-    // Capturar información
-    var datosParaImprimir = {
-      IDContrato: $("#HUD").val(),
-      Fecha_Con: $("#selectedDate").val(),
-      NumeroRadicado_Con: "R23-" + $("#HUD").val(),
-      PuntoInstalacion_Con: $("#puntosI").val(),
-      numSum: "1" + $("#iddomicilio").val(),
-      estado: "En revision",
-      IDDomicilio: $("#iddomicilio").val(),
-      DNI_cli: $("#dniCliente").val(),
-      DNI_Em: $("#asesorSelect").val(),
-      IDGabineteCategoria: $("#gabineteSelect").val(),
-      IDTipoInst: $("#tipoInsSelect").val(),
-    };
+	//
+	$("#imprimir").on("click", function () {
+		// Capturar información
+		var datosParaImprimir = {
+			IDContrato: $("#HUD").val(),
+			Fecha_Con: $("#selectedDate").val(),
+			NumeroRadicado_Con: "R23-" + $("#HUD").val(),
+			PuntoInstalacion_Con: $("#puntosI").val(),
+			numSum: "1" + $("#iddomicilio").val(),
+			estado: "En revision",
+			IDDomicilio: $("#iddomicilio").val(),
+			DNI_cli: $("#dniCliente").val(),
+			DNI_Em: $("#asesorSelect").val(),
+			IDGabineteCategoria: $("#gabineteSelect").val(),
+			IDTipoInst: $("#tipoInsSelect").val(),
+		};
 
-    console.log("Datos para imprimir:", datosParaImprimir);
+		console.log("Datos para imprimir:", datosParaImprimir);
 
-    // Enviar la información al controlador mediante Ajax
-    $.ajax({
-          url: "controlpdf/enviarPDF",
-          type: "POST",
-          contentType: "application/json",
-          data: JSON.stringify(datosParaImprimir),
-          dataType: "json",
-          success: function (response) {
-              console.log(response);
-              // Descomenta esta línea si deseas redirigir después de la solicitud AJAX
-              window.location.href = "controlpdf";
-          },
-          error: function (error) {
-              console.error(error);
-          },
-        });
-    });
+		// Enviar la información al controlador mediante Ajax
+		$.ajax({
+			url: "controlpdf/enviarPDF",
+			type: "POST",
+			contentType: "application/json",
+			data: JSON.stringify(datosParaImprimir),
+			dataType: "json",
+			success: function (response) {
+				console.log(response);
+				// Descomenta esta línea si deseas redirigir después de la solicitud AJAX
+				window.location.href = "controlpdf";
+			},
+			error: function (error) {
+				console.error(error);
+			},
+		});
+	});
 
-  //LIMPIAR
-  $("#btnLimpiar").on("click", function () {
-    $("#HUD").val("");
-    $("#selectedDate").prop("selectedIndex", 0);
-    $("#HUD").val("");
-    $("#puntosI").val("");
-    $("#iddomicilio").val("");
-    $("#iddomicilio").val("");
-    $("#dniCliente").val("");
-    $("#asesorSelect").prop("selectedIndex", 0);
-    $("#gabineteSelect").prop("selectedIndex", 0);
-    $("#tipoInsSelect").prop("selectedIndex", 0);
-  });
+	//LIMPIAR
+	$("#btnLimpiar").on("click", function () {
+		$("#HUD").val("");
+		$("#selectedDate").prop("selectedIndex", 0);
+		$("#HUD").val("");
+		$("#puntosI").val("");
+		$("#iddomicilio").val("");
+		$("#iddomicilio").val("");
+		$("#dniCliente").val("");
+		$("#asesorSelect").prop("selectedIndex", 0);
+		$("#gabineteSelect").prop("selectedIndex", 0);
+		$("#tipoInsSelect").prop("selectedIndex", 0);
+	});
 
-  //BUSCAR
-  $("#formularioRC").submit(function (event) {
-    event.preventDefault();
-    var DNI = $("#dniCliente").val();
-    console.log("Me presione");
-    //Envia el dato mediante AJAX
+	//BUSCAR
+	$("#formularioRC").submit(function (event) {
+		event.preventDefault();
+		var DNI = $("#dniCliente").val();
 
-    $.ajax({
-      url: "registrarContrato/buscarCliente",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(DNI),
-      success: function (response) {
-        var cliente = JSON.parse(response);
+		//Envia el dato mediante AJAX
 
-        if (cliente.Nombre_cli == undefined) {
-          $("#dniCliente").val("");
-          $("input[name='nombrecli']").val("");
-          $("input[name='nombrecli']").val("");
-          $("input[name='direcDomicilio']").val("");
-          $("input[name='iddomicilio']").val("");
-          alert("Cliente no encontrado, por favor verifique el DNI");
-        } else {
-          // Actualiza el valor del campo de texto
-          $("input[name='nombrecli']").val(cliente.DNI_cli);
-          $("input[name='nombrecli']").val(
-            cliente.Nombre_cli + " " + cliente.Apellido_cli
-          );
-          $("input[name='direcDomicilio']").val(cliente.Direccion_Dom);
-          $("input[name='iddomicilio']").val(cliente.IDDomicilio);
-        }
-      },
-      error: function (error) {
-        console.error(error);
-      },
-    });
-  });
+		$.ajax({
+			url: "registrarContrato/buscarCliente",
+			type: "POST",
+			contentType: "application/json",
+			data: JSON.stringify(DNI),
+			success: function (response) {
+				var cliente = JSON.parse(response);
 
-  //REGISTRAR
-  $("#formularioRC1").submit(function (event) {
-    event.preventDefault();
-    var IDContrato = $("#HUD").val();
-    var Fecha_Con = $("#selectedDate").val();
-    var NumeroRadicado_Con = "R23-" + $("#HUD").val();
-    var PuntoInstalacion_Con = $("#puntosI").val();
-    var numSum = "1" + $("#iddomicilio").val();
-    var estado = "En revision";
-    var IDDomicilio = $("#iddomicilio").val();
-    var DNI_cli = $("#dniCliente").val();
-    var DNI_Em = $("#asesorSelect").val();
-    var IDGabineteCategoria = $("#gabineteSelect").val();
-    var IDTipoInst = $("#tipoInsSelect").val();
+				if (cliente.Nombre_cli == undefined) {
+					$("#dniCliente").val("");
+					$("input[name='nombrecli']").val("");
+					$("input[name='nombrecli']").val("");
+					$("input[name='direcDomicilio']").val("");
+					$("input[name='iddomicilio']").val("");
+					alert("Cliente no encontrado, por favor verifique el DNI");
+				} else {
+					// Actualiza el valor del campo de texto
+					$("input[name='nombrecli']").val(cliente.DNI_cli);
+					$("span#mostrarNombres").text(
+						cliente.Nombre_cli + " " + cliente.Apellido_cli
+					);
+					$("input[name='nombrecli']").val(
+						cliente.Nombre_cli + " " + cliente.Apellido_cli
+					);
+					$("input[name='direcDomicilio']").val(cliente.Direccion_Dom);
+					$("span#mostrarDomicilio").text(cliente.Direccion_Dom);
+					$("input[name='iddomicilio']").val(cliente.IDDomicilio);
+					$("span#mostrarIDDom").text(cliente.IDDomicilio);
+				}
+			},
+			error: function (error) {
+				console.error(error);
+			},
+		});
+	});
 
-    var contratoActual = {
-      IDContrato: IDContrato,
-      Fecha_Con: Fecha_Con,
-      NumeroRadicado_Con: NumeroRadicado_Con,
-      PuntoInstalacion_Con: PuntoInstalacion_Con,
-      numSum: numSum,
-      estado: estado,
-      IDDomicilio: IDDomicilio,
-      DNI_cli: DNI_cli,
-      DNI_Em: DNI_Em,
-      IDGabineteCategoria: IDGabineteCategoria,
-      IDTipoInst: IDTipoInst,
-    };
+	//REGISTRAR
+	$("#formularioRC1").submit(function (event) {
+		event.preventDefault();
+		var IDContrato = $("#HUD").val();
+		var Fecha_Con = $("#selectedDate").val();
+		var NumeroRadicado_Con = "R23-" + $("#HUD").val();
+		var PuntoInstalacion_Con = $("#puntosI").val();
+		var numSum = "1" + $("#iddomicilio").val();
+		var estado = "En revision";
+		var IDDomicilio = $("#iddomicilio").val();
+		var DNI_cli = $("#dniCliente").val();
+		var DNI_Em = $("#asesorSelect").val();
+		var IDGabineteCategoria = $("#gabineteSelect").val();
+		var IDTipoInst = $("#tipoInsSelect").val();
 
-    console.log(contratoActual);
-    // Envía el formulario mediante Ajax
+		var contratoActual = {
+			IDContrato: IDContrato,
+			Fecha_Con: Fecha_Con,
+			NumeroRadicado_Con: NumeroRadicado_Con,
+			PuntoInstalacion_Con: PuntoInstalacion_Con,
+			numSum: numSum,
+			estado: estado,
+			IDDomicilio: IDDomicilio,
+			DNI_cli: DNI_cli,
+			DNI_Em: DNI_Em,
+			IDGabineteCategoria: IDGabineteCategoria,
+			IDTipoInst: IDTipoInst,
+		};
 
-    $.ajax({
-      url: "registrarContrato/registrarContrato",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(contratoActual),
-      success: function (response) {
-        // Manejar la respuesta del servidor (si es necesario)
-        alert(response);
-        $("#HUD").val("");
-        $("#selectedDate").prop("selectedIndex", 0);
-        $("#HUD").val("");
-        $("#puntosI").val("");
-        $("#iddomicilio").val("");
-        $("#iddomicilio").val("");
-        $("#dniCliente").val("");
-        $("#asesorSelect").prop("selectedIndex", 0);
-        $("#gabineteSelect").prop("selectedIndex", 0);
-        $("#tipoInsSelect").prop("selectedIndex", 0);
-      },
-      error: function (error) {
-        // Manejar errores
-        console.error(error);
-      },
-    });
-  });
+		console.log(contratoActual);
+		// Envía el formulario mediante Ajax
+
+		$.ajax({
+			url: "registrarContrato/registrarContrato",
+			type: "POST",
+			contentType: "application/json",
+			data: JSON.stringify(contratoActual),
+			success: function (response) {
+				// Manejar la respuesta del servidor (si es necesario)
+				alert(response);
+				$("#HUD").val("");
+				$("#selectedDate").prop("selectedIndex", 0);
+				$("#HUD").val("");
+				$("#puntosI").val("");
+				$("#iddomicilio").val("");
+				$("#iddomicilio").val("");
+				$("#dniCliente").val("");
+				$("#asesorSelect").prop("selectedIndex", 0);
+				$("#gabineteSelect").prop("selectedIndex", 0);
+				$("#tipoInsSelect").prop("selectedIndex", 0);
+			},
+			error: function (error) {
+				// Manejar errores
+				console.error(error);
+			},
+		});
+	});
 });
--- views/registrarContrato/formRegistrarContrato.php
+++ views/registrarContrato/formRegistrarContrato.php
@@ -30,18 +30,20 @@
                     <i class="mdi mdi-account-search"></i>
                 </div>
                 <div class="grupo1">
-                    <label for="">DNI</label>
-                    <input type="text" label="" placeholder="DNI" name="dniCliente" id="dniCliente">
+                    <label for="dniCliente">DNI</label>
+                    <input type="text" placeholder="DNI" name="dniCliente" id="dniCliente">
                 </div>
                 <div class="grupo2">
                     <label for="">Nombres y Apellidos del usuario</label>
                     <input type="text" label="" placeholder="Nombre Completo" name="nombrecli" id="nombrecli" readonly>
                     <label for="">Domicilio del usuario</label>
-                    <input type="text" label="" placeholder="Dirección Domicilio" name="direcDomicilio" id="direcDomicilio" readonly>
+                    <input type="text" label="" placeholder="Dirección Domicilio" name="direcDomicilio"
+                        id="direcDomicilio" readonly>
                     <label for="">ID Domicilio</label>
                     <input type="text" label="" placeholder="ID Domicilio" name="iddomicilio" id="iddomicilio" readonly>
                 </div>
-                <form method="POST" action="<?php echo constant('URL'); ?>registrarContrato/buscarCliente" class="formularioRC" name="formularioRC" id="formularioRC">
+                <form method="POST" action="<?php echo constant('URL'); ?>registrarContrato/buscarCliente"
+                    class="formularioRC" name="formularioRC" id="formularioRC">
                     <div class="boton-cli">
                         <button class="boton-buscar" id="btnBuscar" name="btnBuscar">Buscar Cliente</button>
                     </div>
@@ -52,62 +54,62 @@
         <div class="cont-3">
             <h3 class="sub-contrato">Datos del Contrato</h3>
             <div class="cont-contrato">
-            <div class=grupo1-cont >
-                <label for="">Asesor</label>
-                <select name="asesorSelect" id="asesorSelect" onchange="updateContenedor()">
-                <option value=""></option>
-                <?php
-                            include_once 'models/empleado.php';
-                            foreach ($this->asesores as $opcion) {
-                                $asesor = new Empleado();
-                                $asesor = $opcion; ?>
-                                <option value="<?php echo $opcion->DNI_Em; ?>">
-                                    <?php echo $opcion->Nombre_Em. " ".$opcion->Apellido_Em; ?>
-                                </option>
-                            <?php } ?>
-                </select>
-                <label for="">Número de Radicando</label>
-                <input type="text" label="" placeholder="Escribir..." name="" id="idnumero">
-                <label for="">Tipo de instalación</label>
-                <select name="tipoInsSelect" id="tipoInsSelect"  onchange="updateContenedor()">
-                <option value=""></option>
-                            <?php
-                            include_once 'models/tipoinstalacion.php';
-                            foreach ($this->tipoinstalaciones as $opcion) {
-                                $tipoinstalacion = new TipoInstalacion();
-                                $tipoinstalacion = $opcion; ?>
-                                <option value="<?php echo $opcion->IDTipoInst; ?>">
-                                    <?php echo $opcion->Descripcion_TI; ?>
-                                </option>
-                            <?php } ?>
-                </select>
-                <label for="">Estado</label>
-                <input type="text" label="" placeholder="Estado" name="estado" id="estado" value="En revisión">
-            </div>
-            <div class="grupo2-cont">
-                <label for="">Punto de instalación</label>
-                <input type="number" label="" placeholder="Puntos " name="puntosI" id="puntosI">
-                <label for="">Categoría del Gabinete</label>
-                <select name="gabineteSelect" id="gabineteSelect" onchange="updateContenedor()">
-                <option value=""></option>
-                            <?php
-                            include_once 'models/categoria_gabinete.php';
-                            foreach ($this->gabinetes as $opcion) {
-                                $gabinete = new Categoria_Gabinete();
-                                $gabinete = $opcion; ?>
-                                <option value="<?php echo $opcion->IDGabineteCategoria; ?>">
-                                    <?php echo $opcion->Descripcion_Ga; ?>
-                                </option>
-                            <?php } ?>
-                </select>
-                <label for="">HUD</label>
-                <input type="text" label="" placeholder="HUD" name="HUD" id="HUD">   
-                <label for="">Fecha</label>
-                        <div class="calendario-wrapper">
-                            <input class="calendario" type="date" id="selectedDate" name="selectedDate" min="2000-01-01" max="2023-12-31"
-                                onchange="handleDateSelection(event)">
-                        </div>
-            </div>
+                <div class=grupo1-cont>
+                    <label for="">Asesor</label>
+                    <select name="asesorSelect" id="asesorSelect" onchange="updateContenedor()">
+                        <option value=""></option>
+                        <?php
+                        include_once 'models/empleado.php';
+                        foreach ($this->asesores as $opcion) {
+                            $asesor = new Empleado();
+                            $asesor = $opcion; ?>
+                            <option value="<?php echo $opcion->DNI_Em; ?>">
+                                <?php echo $opcion->Nombre_Em . " " . $opcion->Apellido_Em; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                    <label for="">Número de Radicando</label>
+                    <input type="text" label="" placeholder="Escribir..." name="" id="idnumero">
+                    <label for="">Tipo de instalación</label>
+                    <select name="tipoInsSelect" id="tipoInsSelect" onchange="updateContenedor()">
+                        <option value=""></option>
+                        <?php
+                        include_once 'models/tipoinstalacion.php';
+                        foreach ($this->tipoinstalaciones as $opcion) {
+                            $tipoinstalacion = new TipoInstalacion();
+                            $tipoinstalacion = $opcion; ?>
+                            <option value="<?php echo $opcion->IDTipoInst; ?>">
+                                <?php echo $opcion->Descripcion_TI; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                    <label for="">Estado</label>
+                    <input type="text" label="" placeholder="Estado" name="estado" id="estado" value="En revisión">
+                </div>
+                <div class="grupo2-cont">
+                    <label for="">Punto de instalación</label>
+                    <input type="number" label="" placeholder="Puntos " name="puntosI" id="puntosI">
+                    <label for="">Categoría del Gabinete</label>
+                    <select name="gabineteSelect" id="gabineteSelect" onchange="updateContenedor()">
+                        <option value=""></option>
+                        <?php
+                        include_once 'models/categoria_gabinete.php';
+                        foreach ($this->gabinetes as $opcion) {
+                            $gabinete = new Categoria_Gabinete();
+                            $gabinete = $opcion; ?>
+                            <option value="<?php echo $opcion->IDGabineteCategoria; ?>">
+                                <?php echo $opcion->Descripcion_Ga; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                    <label for="">HUD</label>
+                    <input type="text" label="" placeholder="HUD" name="HUD" id="HUD">
+                    <label for="">Fecha</label>
+                    <div class="calendario-wrapper">
+                        <input class="calendario" type="date" id="selectedDate" name="selectedDate" min="2000-01-01"
+                            max="2023-12-31" onchange="handleDateSelection(event)">
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -122,23 +124,23 @@
                     <h3>Datos Generales del Cliente:</h3>
                     <hr>
                     <div>
-                        <h4>DNI :                         <span id="mostrarDNI"></span>         </h4>
-                        <h4>Nombres y Apellidos :         <span id="mostrarNombres"></span>     </h4>
-                        <h4>Domicilio :                   <span id="mostrarDomicilio"></span>   </h4>
-                        <h4>ID Domicilio :                <span id="mostrarIDDom"></span>       </h4>
+                        <h4>DNI : <span id="mostrarDNI" name="mostrarDNI"></span> </h4>
+                        <h4>Nombres y Apellidos : <span id="mostrarNombres"></span> </h4>
+                        <h4>Domicilio : <span id="mostrarDomicilio"></span> </h4>
+                        <h4>ID Domicilio : <span id="mostrarIDDom"></span> </h4>
                     </div>
                     <br>
                     <h3>Datos del Contrato:</h3>
                     <hr>
                     <div>
-                        <h4>Asesor :                      <span id="mostrarAsesor"></span>      </h4>
-                        <h4>Número de Radicando :         <span id="mostrarNumero"></span>      </h4>
-                        <h4>Tipo de instalación :         <span id="mostrarTipo"></span>        </h4>
-                        <h4>Estado : en revisión          <span id="mostrarEstado"></span>      </h4>
-                        <h4>Punto de instalación :        <span id="mostrarPunto"></span>       </h4>
-                        <h4>Categoría del Gabinete :      <span id="mostrarCategoria"></span>   </h4>
-                        <h4>HUD :                         <span id="mostrarHUD"></span>         </h4>
-                        <h4>Fecha :                       <span id="mostrarFecha"></span>       </h4>
+                        <h4>Asesor : <span id="mostrarAsesor"></span> </h4>
+                        <h4>Número de Radicando : <span id="mostrarNumero"></span> </h4>
+                        <h4>Tipo de instalación : <span id="mostrarTipo"></span> </h4>
+                        <h4>Estado : en revisión <span id="mostrarEstado"></span> </h4>
+                        <h4>Punto de instalación : <span id="mostrarPunto"></span> </h4>
+                        <h4>Categoría del Gabinete : <span id="mostrarCategoria"></span> </h4>
+                        <h4>HUD : <span id="mostrarHUD"></span> </h4>
+                        <h4>Fecha : <span id="mostrarFecha"></span> </h4>
                     </div>
                 </div>
             </div>
@@ -146,18 +148,19 @@
 
         <div class="cont-5">
             <div class="grupo1-boton">
-                <form method="POST" action="<?php echo constant('URL'); ?>registrarContrato/registrarContrato" class="formularioRC1" name="formularioRC1" id="formularioRC1">
+                <form method="POST" action="<?php echo constant('URL'); ?>registrarContrato/registrarContrato"
+                    class="formularioRC1" name="formularioRC1" id="formularioRC1">
                     <button class="boton1" name="btnRegistrar" id="btnRegistrar">
-                    <i class="mdi mdi-book-plus"></i>Registrar</button>
+                        <i class="mdi mdi-book-plus"></i>Registrar</button>
                 </form>
                 <button class="boton1" name="btnLimpiar" id="btnLimpiar">
                     <i class="mdi mdi-restore"></i>Limpiar</button>
                 <button class="boton1" name="btnAtras" id="btnAtras">
                     <i class="mdi mdi-keyboard-backspace"></i>Atras</button>
                 <button class="boton1" id="imprimir">
-                    
+
                     <i class="mdi mdi-printer"></i>
-                    Imprimir                
+                    Imprimir
                 </button>
             </div>
             <br>
@@ -167,83 +170,14 @@
             <br>
             <br>
         </div>
-        
+
         <?php require_once "views/footer.php" ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.1/dist/sweetalert2.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="<?php echo constant('URL'); ?>public/js/registrarContrato.js"></script>
-
-    
+    <script src="<?php echo constant('URL'); ?>public/js/contrato.js"></script>
 </body>
 
-
 </html>
-
-<script>
-    //DATOS CLIENTE
-    const DNIInput = document.getElementById("dniCliente");
-    const mostrarDNI = document.getElementById("mostrarDNI");
-
-    const NombresInput = document.getElementById("nombrecli");
-    const mostrarNombres = document.getElementById("mostrarNombres");
-
-    //DATOS DEL CONTRATO
-    const NumeroInput = document.getElementById("idnumero");
-    const mostrarNumero = document.getElementById("mostrarNumero");
-
-    const puntoInput = document.getElementById("puntosI");
-    const mostrarPunto = document.getElementById("mostrarPunto");
-
-    const hudInput = document.getElementById("HUD");
-    const mostrarHUD = document.getElementById("mostrarHUD");
-
-    const fechaInput = document.getElementById("selectedDate");
-    const mostrarFecha = document.getElementById("mostrarFecha");
-
-
-        // Datos CLIENTE
-        DNIInput.addEventListener("input", () => {
-            mostrarDNI.textContent = DNIInput.value;
-        });
-
-        NombresInput.addEventListener("input", () => {
-            mostrarNombres.textContent = NombresInput.value;
-        });
-
-        //Datos CONTRATO
-        NumeroInput.addEventListener("input", () => {
-            mostrarNumero.textContent = NumeroInput.value;
-        });
-
-        puntoInput.addEventListener("input", () => {
-            mostrarPunto.textContent = puntoInput.value;
-        });
-
-        hudInput.addEventListener("input", () => {
-            mostrarHUD.textContent = hudInput.value;
-        });
-
-        fechaInput.addEventListener("input", () => {
-            mostrarFecha.textContent = fechaInput.value;
-        });
-
-    
-    //Datos contrato Select
-    function updateContenedor(){
-        var asesoresSelect = document.getElementById("asesorSelect");
-        var mostrarAsesor = document.getElementById("mostrarAsesor");
-        mostrarAsesor.textContent = asesoresSelect.options[asesoresSelect.selectedIndex].text;
-
-        var tipoSelect = document.getElementById("tipoInsSelect");
-        var mostrarTipo = document.getElementById("mostrarTipo");
-        mostrarTipo.textContent = tipoSelect.options[tipoSelect.selectedIndex].text;
-
-        var categoriaSelect = document.getElementById("gabineteSelect");
-        var mostrarCategoria = document.getElementById("mostrarCategoria");
-        mostrarCategoria.textContent = categoriaSelect.options[categoriaSelect.selectedIndex].text;
-    }
-
-
-</script>
