9858bc4ffb671e8614c5d720c7d49a7957b34c68
carlosalcedo09 <carlosalcedojavier@gmail.com>
2023-12-05 20:29:09 -0500

    FILTROSSS LOGRADOS

--- controllers/catalogo.php
+++ controllers/catalogo.php
@@ -13,14 +13,12 @@ class Catalogo extends Controller
         } else {
             $this->view->nombrecliente = null;
         }
-
     }
 
     function render()
     {
-        $producto = $this->model->getP();
-
-        $this->view->producto = $producto;
+        $productos = $this->model->getP();
+        $this->view->productos = $productos;
         $this->view->render('portalCatalogo/portalCatalogo');
     }
 
@@ -31,10 +29,13 @@ class Catalogo extends Controller
         $response = array();
 
         if ($datos) {
-            $producto["nombre"] = $datos["nombre"];
-            $producto["cuota"] = $datos["cuota"];
-            $producto["precio1"] = $datos["precio1"];
-            $producto["precio2"] = $datos["precio2"];
+            $producto = [
+                "nombre" => $datos["nombre"],
+                "cuota" => $datos["cuota"],
+                "precio1" => $datos["precio1"],
+                "precio2" => $datos["precio2"]
+            ];
+
             $_SESSION["carrito"][$producto["nombre"]] = $producto;
             $response['success'] = true;
             $response['message'] = 'Producto agregado al carrito';
@@ -42,7 +43,54 @@ class Catalogo extends Controller
             $response['success'] = false;
             $response['message'] = 'No se pudo agregar el producto';
         }
+
         header('Content-Type: application/json');
         echo json_encode($response);
+    }
+
+    function filtrado()
+    {
+        $datosFiltrado = json_decode(file_get_contents("php://input"));
+        $subcategorias = $datosFiltrado->subcategorias;
+        $marcas = $datosFiltrado->marcas;
+
+        if (!empty($subcategorias) || !empty($marcas)) {
+            $productos = $this->model->getFiltrado($subcategorias, $marcas);
+        } else {
+            $productos = $this->model->getP();
+        }
+
+        $this->view->productos = $productos;
+        $this->view->render('portalCatalogo/portalCatalogoR');
+    }
+
+    function filtrarprecios() {
+        $datosJson = file_get_contents("php://input");
+        $precios = json_decode($datosJson, true);
+        
+        $minPrecio= isset($precios["minPrecio"]) ? $precios["minPrecio"]: 0;
+        $maxPrecio= isset($precios["maxPrecio"]) ? $precios["maxPrecio"]: 0;
+
+        if (!empty($precios)) {
+            $productos = $this->model->getFiltradoPrecio($minPrecio,$maxPrecio);
+        } else {
+            $productos = $this->model->getP();
+        }
+
+        $this->view->productos = $productos;
+        $this->view->render('portalCatalogo/portalCatalogoR');
+    }
+
+    function ordenarProducto(){
+        $datosJson = file_get_contents("php://input");
+        $criterio = json_decode($datosJson, true);
+
+        if(empty($criterio) || $criterio=="todos"){
+            $productos = $this->model->getP();
+        }else{
+            $productos=$this->model->getOrdenar($criterio);
+        }
+            $this->view->productos = $productos;
+        $this->view->render('portalCatalogo/portalCatalogoR');
     }
 }
